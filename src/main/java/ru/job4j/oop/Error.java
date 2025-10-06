f33e36dc07c1ffb03991876bdb23044407be3e4b
Artem <cartem33@gmail.com>
2025-10-06 19:40:01 +0300

    1.7. Конструктор [#173889 #410711]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+
+    }
+
+    public void printInfo() {
+        System.out.println("Состояние: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error first = new Error(true, 1, "Failed");
+        Error second = new Error();
+        first.printInfo();
+        second.printInfo();
+    }
+}
