bd6d6957ea4e60e10cef938c1a735b8ebf066454
Artem <cartem33@gmail.com>
2025-10-08 02:13:08 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642 #410954]

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double semiPerimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double bc, double ac) {
+        return  ab + bc > ac && ac + bc > bc && ab + ac > bc;
+    }
+
+    public double area() {
+        double result = -1;
+        double ab = first.distance(second);
+        double bc = second.distance(third);
+        double ac = first.distance(third);
+        if (this.exist(ab, bc, ac)) {
+            double p = semiPerimeter(ab, bc, ac);
+            result = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+        return result;
+    }
+}
