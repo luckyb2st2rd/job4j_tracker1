63f376d75b4fed95a2c1f598e2b15c75ea124ec9
Artem <cartem33@gmail.com>
2025-10-16 19:43:47 +0300

    3.1. Массив с пустыми ячейками. Рефакторинг

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -15,7 +15,7 @@ public class Shop {
     }
 
     public static int indexOfNull(Product[] products) {
-        int index = 0;
+        int index = -1;
         for (int i = 0; i < products.length; i++) {
             Product product = products[i];
             if (product == null) {
