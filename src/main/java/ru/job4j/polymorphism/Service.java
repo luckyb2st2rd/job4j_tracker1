374d9512acf587e4766054e580cf3a673d41df3b
Artem <cartem33@gmail.com>
2025-10-21 02:12:15 +0300

    1. Что такое полиморфизм.

---
+++ src/main/java/ru/job4j/polymorphism/Service.java
@@ -0,0 +1,22 @@
+package ru.job4j.polymorphism;
+
+import java.io.File;
+
+public class Service {
+    private Store store;
+
+    public Service(Store store) {
+        this.store = store;
+    }
+
+    public void add() {
+        store.save("Artem Chigarov");
+    }
+
+    public static void main(String[] args) {
+        /*MemoryStore store = new MemoryStore();*/
+        FileStore store = new FileStore();
+        Service service = new Service(store);
+        service.add();
+    }
+}
