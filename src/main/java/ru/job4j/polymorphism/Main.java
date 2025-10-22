7f7671437bb6c141f25f2b422d45c9368a877555
Artem <cartem33@gmail.com>
2025-10-23 02:39:56 +0300

    4.0. Что такое Interface.

---
+++ src/main/java/ru/job4j/polymorphism/Main.java
@@ -0,0 +1,17 @@
+package ru.job4j.polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehicle townCar = new TownCar();
+        townCar.accelerate();
+        townCar.brake();
+        townCar.steer();
+        townCar.changeGear();
+        Vehicle sportCar = new SportCar();
+        sportCar.accelerate();
+        sportCar.brake();
+        sportCar.changeGear();
+        sportCar.steer();
+        sportCar.refill();
+    }
+}
