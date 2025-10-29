d7ccfd04ae01595abc16220172ec2a3c16ccd24b
Artem <cartem33@gmail.com>
2025-10-29 18:32:25 +0300

    8. Input и полиморфизм.

--- src/main/java/ru/job4j/tracker/MockInput.java
+++ src/main/java/ru/job4j/tracker/MockInput.java
@@ -3,16 +3,20 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class MockInput implements Input {
+    private String[] answers;
+    private int position = 0;
+
+    public MockInput(String[] answers) {
+        this.answers = answers;
+    }
 
     @Override
-    public String askStr(String question) {
-        Scanner scanner = new Scanner(System.in);
-        question = scanner.nextLine();
-        return question;
+        public String askStr(String question) {
+        return answers[position++];
     }
 
     @Override
     public int askInt(String question) {
-        return 0;
+        return Integer.parseInt(askStr(question));
     }
 }
