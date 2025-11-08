8d483b6d681411ad192fce90150a5e7f55e35a87
Artem <cartem33@gmail.com>
2025-11-09 02:05:12 +0300

    4. Принципы раннего возврата и охранных выражений

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,71 @@
+package ru.job4j.early;
+
+public class PasswordValidator {
+    private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
+
+    public static String validate(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException(
+                    "Password can't be null"
+            );
+        }
+
+        int len = password.length();
+
+        if (len < 8 || len > 32) {
+            throw new IllegalArgumentException(
+                    "Password should be length [8, 32]"
+            );
+        }
+
+        String lower = password.toLowerCase();
+        for (String invalid : FORBIDDEN) {
+            if (lower.contains(invalid)) {
+                throw new IllegalArgumentException(
+                        "Password shouldn't contain substrings: qwerty, 12345, password, admin, user"
+                );
+            }
+        }
+
+        boolean hasUpCase = false;
+        boolean hasLowCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+
+        for (char symbol : password.toCharArray()) {
+            if (Character.isUpperCase(symbol)) {
+                hasUpCase = true;
+            } else if (Character.isLowerCase(symbol)) {
+                hasLowCase = true;
+            } else if (Character.isDigit(symbol)) {
+                hasDigit = true;
+            } else {
+                hasSpecial = true;
+            }
+        }
+
+        if (!hasUpCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one uppercase letter"
+            );
+        }
+        if (!hasLowCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one lowercase letter"
+            );
+        }
+        if (!hasDigit) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one figure"
+            );
+        }
+        if (!hasSpecial) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one special symbol"
+            );
+        }
+
+        return password;
+
+    }
+}
